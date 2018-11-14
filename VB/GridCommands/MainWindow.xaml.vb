9f7a41997a718d3fe079de7b1bd48dae3d18cdef
DevExpressExampleBot <fakeEmail>
2018-11-14 13:21:02 +0300

    Source auto update [skip ci]

--- VB/GridCommands/MainWindow.xaml.vb
+++ VB/GridCommands/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Text
@@ -17,66 +16,68 @@ Imports System.Collections
 Imports System.Collections.ObjectModel
 
 Namespace DxTreeViewTest
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			AddHandler Me.Loaded, AddressOf MainWindow_Loaded
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
 
-		Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			BuildTree()
-			'treeListView.DataContext = treeListView;
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            AddHandler Me.Loaded, AddressOf MainWindow_Loaded
+        End Sub
 
-		Private RootNode As New ObservableCollection(Of TreeListNode)()
-		Private Sub BuildTree()
-			Dim rootNode As TreeListNode = CreateRootNode(New Item() With {.Value = 3, .IsProcessed = False})
-			Dim childNode As TreeListNode = CreateChildNode(rootNode, New Item() With {.Value = 15, .IsProcessed = True})
-			CreateChildNode(childNode, New Item() With {.Value = 6, .IsProcessed = False})
+        Private Sub MainWindow_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            BuildTree()
+            'treeListView.DataContext = treeListView;
+        End Sub
 
-			Dim childNode1 As TreeListNode = CreateChildNode(rootNode, New Item() With {.Value = 3, .IsProcessed = False})
-			CreateChildNode(childNode1, New Item() With {.Value = 9, .IsProcessed = True})
+        Private RootNode As New ObservableCollection(Of TreeListNode)()
+        Private Sub BuildTree()
 
-			'grid.ItemsSource = rootNode;
-		End Sub
+            Dim rootNode_Renamed As TreeListNode = CreateRootNode(New Item() With { _
+                .Value = 3, _
+                .IsProcessed = False _
+            })
+            Dim childNode As TreeListNode = CreateChildNode(rootNode_Renamed, New Item() With { _
+                .Value = 15, _
+                .IsProcessed = True _
+            })
+            CreateChildNode(childNode, New Item() With { _
+                .Value = 6, _
+                .IsProcessed = False _
+            })
 
-		Private Function CreateRootNode(ByVal dataObject As Object) As TreeListNode
-			Dim rootNode As New TreeListNode(dataObject)
-			treeListView.Nodes.Add(rootNode)
-			Me.RootNode.Add(rootNode)
-			Return rootNode
-		End Function
+            Dim childNode1 As TreeListNode = CreateChildNode(rootNode_Renamed, New Item() With { _
+                .Value = 3, _
+                .IsProcessed = False _
+            })
+            CreateChildNode(childNode1, New Item() With { _
+                .Value = 9, _
+                .IsProcessed = True _
+            })
 
-		Private Function CreateChildNode(ByVal parentNode As TreeListNode, ByVal dataObject As Object) As TreeListNode
-			Dim childNode As New TreeListNode(dataObject)
-			parentNode.Nodes.Add(childNode)
-			Return childNode
-		End Function
-	End Class
+            'grid.ItemsSource = rootNode;
+        End Sub
 
-	Public Class Item
-		Private privateValue As Integer
-		Public Property Value() As Integer
-			Get
-				Return privateValue
-			End Get
-			Set(ByVal value As Integer)
-				privateValue = value
-			End Set
-		End Property
-		Private privateIsProcessed As Boolean
-		Public Property IsProcessed() As Boolean
-			Get
-				Return privateIsProcessed
-			End Get
-			Set(ByVal value As Boolean)
-				privateIsProcessed = value
-			End Set
-		End Property
-	End Class
+        Private Function CreateRootNode(ByVal dataObject As Object) As TreeListNode
+
+            Dim rootNode_Renamed As New TreeListNode(dataObject)
+            treeListView.Nodes.Add(rootNode_Renamed)
+            RootNode.Add(rootNode_Renamed)
+            Return rootNode_Renamed
+        End Function
+
+        Private Function CreateChildNode(ByVal parentNode As TreeListNode, ByVal dataObject As Object) As TreeListNode
+            Dim childNode As New TreeListNode(dataObject)
+            parentNode.Nodes.Add(childNode)
+            Return childNode
+        End Function
+    End Class
+
+    Public Class Item
+        Public Property Value() As Integer
+        Public Property IsProcessed() As Boolean
+    End Class
 
 End Namespace
